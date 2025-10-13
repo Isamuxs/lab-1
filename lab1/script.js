26c12c7b3e90c67bcf4b2058b4d04ceeef623faa
Isamuxs <61750763+Isamuxs@users.noreply.github.com>
2025-10-13 10:50:53 +0300

    Update script.js

--- lab1/script.js
+++ lab1/script.js
@@ -2,11 +2,11 @@
 console.log(
   "Usage: triangle(value1, type1, value2, type2);\n" +
   "Types:\n" +
-  "  'leg'             – катет\n" +
-  "  'hypotenuse'      – гіпотенуза\n" +
-  "  'adjacent angle'  – кут, прилеглий до вказаного катета\n" +
-  "  'opposite angle'  – кут, протилежний до вказаного катета\n" +
-  "  'angle'           – гострий кут\n"
+  "  'leg'              катет\n" +
+  "  'hypotenuse'       гіпотенуза\n" +
+  "  'adjacent angle'   кут, прилеглий до вказаного катета\n" +
+  "  'opposite angle'   кут, протилежний до вказаного катета\n" +
+  "  'angle'            гострий кут\n"
 );
 
 function triangle(v1, t1, v2, t2) {
@@ -36,8 +36,8 @@ function triangle(v1, t1, v2, t2) {
       break;
 
     case 'hypotenuse|leg':
-      c = type1==='hypotenuse'? v1 : v2;
-      a = type1==='leg'? v1 : v2;
+      c = type1 === 'hypotenuse'? v1 : v2;
+      a = type1 === 'leg'? v1 : v2;
       if (a >= c) return console.log("Cathetus must be less than hypotenuse"), "Invalid dimensions";
       b = Math.sqrt(c*c - a*a);
       alpha = Math.asin(a/c) * 180/Math.PI;
@@ -45,8 +45,8 @@ function triangle(v1, t1, v2, t2) {
       break;
 
     case 'angle|hypotenuse':
-      c = type1==='hypotenuse'? v1 : v2;
-      alpha = type1==='angle'? v1 : v2;
+      c = type1 === 'hypotenuse'? v1 : v2;
+      alpha = type1 === 'angle'? v1 : v2;
       if (!validAngle(alpha)) return console.log("Angle must be between 0 and 90"), "Invalid angle";
       {
         const rad = toRad(alpha);
@@ -58,8 +58,8 @@ function triangle(v1, t1, v2, t2) {
 
     case 'adjacent angle|leg':
       {
-        const legAdj = type1==='leg'? v1 : v2;
-        const angAdj = type1==='adjacent angle'? v1 : v2;
+        const legAdj = type1 === 'leg'? v1 : v2;
+        const angAdj = type1 === 'adjacent angle'? v1 : v2;
         if (!validAngle(angAdj)) return console.log("Angle must be between 0 and 90"), "Invalid angle";
         const radA = toRad(angAdj);
         b = legAdj;
@@ -72,8 +72,8 @@ function triangle(v1, t1, v2, t2) {
 
     case 'leg|opposite angle':
       {
-        const legOpp = type1==='leg'? v1 : v2;
-        const angOpp = type1==='opposite angle'? v1 : v2;
+        const legOpp = type1 === 'leg'? v1 : v2;
+        const angOpp = type1 === 'opposite angle'? v1 : v2;
         if (!validAngle(angOpp)) return console.log("Angle must be between 0 and 90"), "Invalid angle";
         const radO = toRad(angOpp);
         a = legOpp;
